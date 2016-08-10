0b80f5efa39d8ef2ec8f1a918cb3faaf4e4068c2
Jose Alvaro Garcia <jag040393@gmail.com>
2016-08-09 20:12:40 -0700

    Added css styling and stop-go capabilities

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,46 @@
-var food = ["pizza","sushi","pasta","fried chicken"];
+var food = ["pizza","sushi","pasta","fried chicken","tacos","burritos"];
 
 
 
 
-$("body").on('click', ".food", function(){
+$("body").on("click", ".food", function(){
 	$("#showGif").empty();
 	var food = $(this).attr("data-value");
-	var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=dc6zaTOxFJmzC&limit=24";
+	var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + food + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 $.ajax({url: queryUrl, method: 'GET'}).done(function(response) {
 	var x = response.data;
-
-
 	for(var i=0; i < x.length;i++){
-		var foodImageDiv = $("<div class='foodImage well col-sm-3'>");
+		var foodImageDiv = $("<div class='panel col-sm-4'>");
 		var foodImage = $("<img>")
+		var foodImageRating = x[i].rating;
 		foodImage.attr("src", x[i].images.fixed_height.url);
-		var appendedDiv = foodImageDiv.append(foodImage)
+		foodImage.attr("data-state", "still");
+		foodImage.attr("data-still", x[i].images.fixed_height_still.url);
+		foodImage.attr("data-animate", x[i].images.fixed_height.url);
+		foodImage.addClass("foodImage")
+		var appendedDiv = foodImageDiv.append(foodImage);
+			appendedDiv.append("<h4>" + "RATING:"+ " "+ foodImageRating + "</h4>");
 		$("#showGif").prepend(appendedDiv);
 
 
-// APPEND THE PAUSE AND GO HERE
-
 	}
-
-
-
-
-
-
 });
-
 });
 
 
 
 
-
-
-
-
+$("body").on("click", ".foodImage", function(){
+	 var state = $(this).attr("data-state");
+	   if (state === "still"){
+                $(this).attr("src", $(this).data("animate"));
+                $(this).attr("data-state", "animate");
+            }else{
+                $(this).attr("src", $(this).data("still"));
+                $(this).attr("data-state", "still");
+            } 
+});
 
 
 
@@ -64,7 +65,6 @@ $("#addButton").on("click", function(){
 		food.push(foodInput);
 
 		makeButton();
-
 		return false;
 
 });
